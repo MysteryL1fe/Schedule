88cf74c22ecdd31ace2e3634b5c484c0abec7d19
Khanin Dmitriy <dmitrii.khanin@gmail.com>
2023-07-09 11:16:10 +0300

    cleared ScheduleStorage.java

--- app/src/main/java/com/example/schedule/ScheduleDBHelper.java
+++ app/src/main/java/com/example/schedule/ScheduleDBHelper.java
@@ -9,7 +9,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.os.Environment;
-import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
